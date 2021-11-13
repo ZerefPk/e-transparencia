473221140929601eb30f3edff542f794d51ca774
Ezequiel Nascimento <50809136+ZerefPk@users.noreply.github.com>
2021-11-13 15:49:54 -0300

    List bidding

--- app/Http/Livewire/Dashboard/Bidding/Index.php
+++ app/Http/Livewire/Dashboard/Bidding/Index.php
@@ -13,17 +13,65 @@ class Index extends Component
 
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
+    public $number = null;
+    public $modality = null;
+    public $status = null;
+    public $year = null;
 
+    public $queryString = ['number','modality','year','status'];
+
+    public function find()
+    {
+
+        $biddings = Bidding::query();
+
+        if (isset($this->number)) {
+            $biddings->where('number', 'like', '%'.$this->number);
+
+        }
+        if (isset($this->modality)) {
+            $category = Category::where('slug',  $this->modality)->first();
+            if($category)
+            {
+                $biddings->where('modality_id', $category->id);
+            }
+
+        }
+        if (isset($this->status) && $this->status != "") {
+
+            $biddings->where('status', $this->status);
+
+
+        }
+        if (isset($this->year)) {
+            $biddings->where('year', $this->year);
+        }
+
+        $biddings = $biddings->orderBy('year', 'DESC')->orderBy('number', 'ASC');
+
+
+
+
+        return $biddings;
+
+
+
+    }
+    public function resetPage()
+    {
+        return redirect()->route('dashboard.bidding.index');
+    }
     public function render()
     {
 
         $categories = Category::where('status', true)->where('type','like','bidding_%' )->get();
         $years = Year::where('status', true)->orderBy('year', 'DESC')->get();
-
+        $search = $this->find();
         return view('livewire.dashboard.bidding.index', [
-            'biddings' => Bidding::paginate(5),
+            'biddings' => $search->paginate(10),
             'years' => $years,
             'categories' => $categories,
         ]);
     }
+
 }
--- resources/views/livewire/dashboard/bidding/index.blade.php
+++ resources/views/livewire/dashboard/bidding/index.blade.php
@@ -14,129 +14,110 @@
     </div><!-- /.row -->
 @stop
 
-<div class="row">
-    <div class="col-md-4">
+<div class="card card-primary card-outline">
+    <div class="card-header">
 
-        <div class="card card-primary">
-            <div class="card-header">
-                <h3 class="card-title">Buscar:</h3>
-            </div>
-            <form  method="GET">
-                <div class="card-body">
+        <h3 class="card-title">Licitações:
+        </h3>
 
-                    <div class="form-group">
-                        <label>Ano:</label>
-                        <select class="form-control" style="width: 100%;" name="ano">
-                            <option selected disabled>Selecione</option>
-                            @foreach ($years as $year)
-                                <option value="{{ $year->year }}">{{ $year->year }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <div class="input-group input-group-lg">
-                            <input type="text" class="form-control" placeholder="Processo ou Objeto" name="q">
-                            <div class="input-group-append">
-                                <button type="submit" class="input-group-text"><i class="fa fa-search"></i></button>
+        <div class="card-tools">
+            <a href="{{ route('dashboard.bidding.create') }}" class="btn btn-primary btn-block"><i
+                    class="fa fa-plus"></i>
+                Novo</a>
+
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th style="width: 10px">#</th>
+                        <th>Processo</th>
+                        <th>Modalidade</th>
+                        <th>Status</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> #</td>
+                        <td>
+                            <div class="row">
+                                <div class="col">
+                                    <select class="form-control" style="width: 100%;" name="modality" wire:model="year">
+                                        <option value="" selected>Ano</option>
+                                        @foreach ($years as $year)
+                                            <option value="{{ $year->year }}">{{ $year->year }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col">
+                                    <input type="number" class="form-control" placeholder="Processo" wire:model="number">
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label>Modalidade:</label>
-                        <select class="form-control" style="width: 100%;" name="modalidade">
-                            <option selected disabled>Modalidade</option>
-                            @foreach ($categories->where('type', 'bidding_modality') as $modality)
-                                <option value="{{ $modality->slug }}">{{ $modality->category }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Situação:</label>
-                        <select class="form-control" style="width: 100%;" name="situacao">
-                            <option selected disabled>Situação</option>
-                            @foreach ($categories->where('type', 'bidding_situation') as $situation)
-                                <option value="{{ $situation->slug }}">{{ $situation->category }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Tipo: </label>
-                        <select class="form-control" style="width: 100%;" name="tipo">
-                            <option selected disabled>Tipo de licitação</option>
-                            @foreach ($categories->where('type', 'bidding_type') as $type)
-                                <option value="{{ $type->slug }}">{{ $type->category }}</option>
-                            @endforeach
-                        </select>
-                    </div>
+                        </td>
+                        <td>
 
-                </div>
-                <div class="card-footer clearfix">
-                    <div class="row">
-
-                        <button type="submit" class="btn btn-primary btn-block"><i class="fa fa-search"></i>
-                            Buscar</button>
-                    </div>
-                </div>
-                <!-- /.card-body -->
-            </form>
-        </div>
-
-    </div>
-    <div class="col-md-8">
-
-        <div class="card card-primary card-outline">
-            <div class="card-header">
-                <h3 class="card-title">Todas as licitações</h3>
-
-                <div class="card-tools">
-                    <a href="{{ route('dashboard.bidding.create') }}" class="btn btn-primary btn-block"><i
-                            class="fa fa-plus"></i>
-                        Novo</a>
-
-                </div>
-            </div>
-            <div class="card-body">
-                <table class="table">
-                    <thead>
+                            <select class="form-control" style="width: 100%;" name="modality" wire:model="modality">
+                                <option value="" selected>Modalidade</option>
+                                @foreach ($categories->where('type', 'bidding_modality') as $modality)
+                                    <option value="{{ $modality->slug }}">{{ $modality->category }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </td>
+                        <td>
+                            <select name="status" class="form-control" style="width: 100%;" wire:model="status">
+                                <option value="">status</option>
+                                <option value="1">Publicado</option>
+                                <option value="0">Não publicado</option>
+                            </select>
+                        </td>
+                        <td>
+                            <button wire:click="resetPage()" class="btn btn-warning"><i
+                                    class="fa fa-broom"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    @forelse ($biddings as $bidding)
                         <tr>
-                            <th style="width: 10px">#</th>
-                            <th>Processo</th>
-                            <th>Modalidade</th>
-                            <th style="width: 40px">Status</th>
-                            <th style="width: 40px">Ação</th>
+                            <td class="text-center">{{ $bidding->id }}</td>
+                            <td  class="text-center">{{ $bidding->year }}/{{ $bidding->number }}</td>
+                            <td  class="text-center">
+                                {{ $bidding->modality->category }}
+                            </td>
+                            <td class="text-center">
+                                @if ($bidding->status)
+                                    <span class="badge bg-success"> <i class="fa fa-eye"></i></span>
+                                @else
+                                    <span class="badge bg-danger"> <i class="fa fa-eye-slash"></i></span>
+                                @endif
+                            </td>
+                            <td  class="text-center"><span class="badge bg-primary"> <a href="">
+                                        <i class="fa fa-edit"></i>
+                                    </a></span></td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($biddings as $bidding)
-                            <tr>
-                                <td>{{ $bidding->id }}</td>
-                                <td>{{ $bidding->year }}/{{ $bidding->number }}</td>
-                                <td>
-                                    {{ $bidding->modality->category }}
-                                </td>
-                                <td>
-                                    @if ($bidding->status)
-                                        <span class="badge bg-success"> <i class="fa fa-eye"></i></span>
-                                    @else
-                                        <span class="badge bg-danger"> <i class="fa fa-eye-slash"></i></span>
-                                    @endif
-                                </td>
-                                <td><span class="badge bg-primary"> <a href="">
-                                            <i class="fa fa-edit"></i>
-                                        </a></span></td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <!-- /.card-body -->
-            <div class="card-footer clearfix">
-                <ul class="pagination pagination-sm m-0 float-right">
-                    {!! $biddings->links() !!}
-                </ul>
-            </div>
+                    @empty
+                        <tr>
+                            <td colspan="5" class="text-center text-uppercase">
+                                não há registro para o filtro de busca aplicado
+                            </td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
-        <!-- /.card -->
     </div>
-
+    <!-- /.card-body -->
+    <div class="card-footer clearfix">
+        <ul class="pagination pagination-sm m-0 float-right">
+            {!! $biddings->links() !!}
+        </ul>
+    </div>
 </div>
+<!-- /.card -->
+@section('css')
+
+@stop
